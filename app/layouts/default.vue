1038cc54bccc3de0308efb6f9ceddaf9b5df2c18
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-22 01:30:23 +0700

    feat: add footer components, wrap page components with UMain component on default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import Header from "~/components/layouts/Header.vue";
+import Footer from "~/components/layouts/Footer.vue";
 </script>
 
 <template>
   <UApp>
     <Header />
-    <slot />
+    <UMain>
+      <slot />
+    </UMain>
+    <Footer />
   </UApp>
 </template>
