8fccab0574ec9bbc41edee45a23c1b15ce82d444
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-24 14:18:58 +0700

    docs: add code documentation on several codes

--- app/lib/fetcher.ts
+++ app/lib/fetcher.ts
@@ -1,6 +1,13 @@
 import type { NitroFetchOptions } from "nitropack";
 import type { ApiFailed, ApiSuccess } from "~/types/api";
-
+/**
+ * Fetches data from the API.
+ *
+ * @template T - The type of the data returned by the API.
+ * @param {string} url - The URL of the API endpoint.
+ * @param {NitroFetchOptions<any>} [options={}] - The options for the fetch request.
+ * @return {Promise<ApiFailed | ApiSuccess<T>>} - A promise that resolves to the API response.
+ */
 export async function apiFetch<T>(
   url: string,
   options: NitroFetchOptions<any> = {},
--- app/types/api.ts
+++ app/types/api.ts
@@ -1,18 +1,62 @@
+/**
+ * Represents the response from the API.
+ */
 export type ApiResponse = {
+  /**
+   * Indicates whether the request was successful.
+   */
   success: boolean;
 };
 
+/**
+ * Represents a successful API response.
+ *
+ * @template T - The type of data returned by the API.
+ * @template F - The type of additional meta data returned by the API (optional).
+ */
 export type ApiSuccess<T, F = unknown> = ApiResponse & {
+  /**
+   * Indicates that the request was successful.
+   */
   success: true;
+
+  /**
+   * The data returned by the API.
+   */
   data: T;
+
+  /**
+   * Additional metadata returned by the API (optional).
+   */
   meta?: F;
 };
 
+/**
+ * Represents a failed API response.
+ */
 export type ApiFailed = ApiResponse & {
+  /**
+   * Indicates that the request failed.
+   */
   success: false;
+
+  /**
+   * The error object containing information about the failure.
+   */
   error: {
+    /**
+     * The type of error.
+     */
     type: string;
+
+    /**
+     * A human-readable message describing the error.
+     */
     message: string;
+
+    /**
+     * Additional details about the error (optional).
+     */
     details?: Record<string, unknown>;
   };
 };
