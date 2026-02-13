0e187892b18a70a86e3c912322dec76ba7c26bef
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-02-13 14:33:06 +0700

    feat: create StateView as a general state UI and update ErrorState to implement StateView component

--- app/components/states/ErrorState.vue
+++ app/components/states/ErrorState.vue
@@ -1,49 +1,30 @@
-<script setup lang="ts">
-defineProps<{
-  title: string;
-  description: string;
-  actionLabel?: string;
-}>();
+<template>
+  <StateView
+    icon="i-heroicons-exclamation-circle"
+    :title="props.title"
+    :description="props.description"
+    :action-label="props.actionLabel"
+    @retry="$emit('retry')"
+  />
+</template>
+
+<script lang="ts" setup>
+import StateView from "./StateView.vue";
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    description?: string;
+    actionLabel?: string;
+  }>(),
+  {
+    description:
+      "Terjadi kendala koneksi atau masalah teknis. Silakan coba lagi nanti.",
+    actionLabel: "Muat Ulang",
+  },
+);
 
 defineEmits<{
   (e: "retry"): void;
 }>();
 </script>
-
-<template>
-  <section
-    class="flex flex-col items-center justify-center max-w-sm md:max-w-md w-full text-center"
-  >
-    <div
-      class="mb-6 flex items-center justify-center rounded-full w-16 h-16 md:w-20 md:h-20 bg-muted"
-    >
-      <UIcon
-        name="i-heroicons-exclamation-circle"
-        class="text-4xl md:text-5xl"
-      />
-    </div>
-
-    <div class="mb-8 flex flex-col space-y-3">
-      <h4 class="font-bold text-xl md:text-2xl">
-        {{ title }}
-      </h4>
-      <p class="text-toned leading-relaxed">
-        {{ description }}
-      </p>
-    </div>
-
-    <UButton
-      v-if="actionLabel"
-      icon="i-lucide-rotate-cw"
-      :label="actionLabel"
-      variant="outline"
-      color="neutral"
-      size="xl"
-      @click="$emit('retry')"
-      :ui="{
-        label: 'text-sm',
-        leadingIcon: 'size-4',
-      }"
-    />
-  </section>
-</template>
---
+++ app/components/states/StateView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+  title: string;
+  description: string;
+  actionLabel?: string;
+}>();
+
+defineEmits<{
+  (e: "retry"): void;
+}>();
+</script>
+
+<template>
+  <section
+    class="flex flex-col items-center justify-center max-w-sm md:max-w-md w-full text-center"
+  >
+    <div
+      class="mb-6 flex items-center justify-center rounded-full w-16 h-16 md:w-20 md:h-20 bg-muted"
+    >
+      <UIcon :name="icon" class="text-4xl md:text-5xl" />
+    </div>
+
+    <div class="mb-8 flex flex-col space-y-3">
+      <h4 class="font-bold text-xl md:text-2xl">
+        {{ title }}
+      </h4>
+      <p class="text-toned leading-relaxed">
+        {{ description }}
+      </p>
+    </div>
+
+    <UButton
+      v-if="actionLabel"
+      icon="i-lucide-rotate-cw"
+      :label="actionLabel"
+      variant="outline"
+      color="neutral"
+      size="xl"
+      @click="$emit('retry')"
+      :ui="{
+        label: 'text-sm',
+        leadingIcon: 'size-4',
+      }"
+    />
+  </section>
+</template>
