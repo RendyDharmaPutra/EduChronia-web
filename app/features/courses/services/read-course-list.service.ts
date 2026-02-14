f1b978b271cee5d18a0aded56d79599af85fe078
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-02-15 01:57:43 +0700

    feat: add meta response structure for pagination & implement it on read course service

--- app/features/courses/services/read-course-list.service.ts
+++ app/features/courses/services/read-course-list.service.ts
@@ -1,10 +1,14 @@
 import { apiFetch } from "~/lib/fetcher";
 import { safeApiCall } from "~/lib/api/safe-api-call";
 import type { Course } from "../course.type";
+import type { MetaPaginationResponse } from "~/types/meta";
 
-export const readCourseListService = (page: number, limit = 10) =>
+export const readCourseListService = async (page: number, limit = 10) =>
   safeApiCall(() =>
-    apiFetch<Course[]>(`/api/courses?page=${page}&limit=${limit}`, {
-      method: "GET",
-    }),
+    apiFetch<Course[], MetaPaginationResponse>(
+      `/api/courses?page=${page}&limit=${limit}`,
+      {
+        method: "GET",
+      },
+    ),
   );
---
+++ app/types/meta.ts
@@ -0,0 +1,7 @@
+export type MetaPaginationResponse = {
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+};
