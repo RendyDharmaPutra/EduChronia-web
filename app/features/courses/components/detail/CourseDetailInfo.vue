54e1582f06c2335d00d519e30860eb39aa44f516
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-02-20 14:18:14 +0700

    feat: add UI action buttons for edit and delete without functionality

--- app/features/courses/components/detail/CourseDetailInfo.vue
+++ app/features/courses/components/detail/CourseDetailInfo.vue
@@ -10,9 +10,18 @@ const hasDescription = !!props.course.description;
 
 <template>
   <section class="flex flex-col space-y-4 w-full">
-    <h2 class="font-extrabold text-4xl lg:text-5xl text-highlighted">
-      {{ course.name }}
-    </h2>
+    <div
+      class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:justify-between w-full h-fit"
+    >
+      <h2 class="font-extrabold text-4xl lg:text-5xl text-highlighted">
+        {{ course.name }}
+      </h2>
+
+      <div class="flex flex-row items-center space-x-2">
+        <UButton icon="i-heroicons-pencil" variant="subtle" color="neutral" />
+        <UButton icon="i-heroicons-trash" variant="subtle" color="error" />
+      </div>
+    </div>
     <p
       :class="`text-base md:text-lg text-muted leading-relaxed max-w-4xl ${!hasDescription && 'italic'}`"
     >
