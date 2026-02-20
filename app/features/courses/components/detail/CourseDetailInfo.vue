94f0c9fcffc3181a552796477e3bf9e0e02f738e
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-02-20 13:58:04 +0700

    feat: handle empty description

--- app/features/courses/components/detail/CourseDetailInfo.vue
+++ app/features/courses/components/detail/CourseDetailInfo.vue
@@ -4,6 +4,8 @@ import type { Course } from "../../course.type";
 const props = defineProps<{
   course: Course;
 }>();
+
+const hasDescription = !!props.course.description;
 </script>
 
 <template>
@@ -11,8 +13,14 @@ const props = defineProps<{
     <h2 class="font-extrabold text-4xl lg:text-5xl text-highlighted">
       {{ course.name }}
     </h2>
-    <p class="text-lg text-muted leading-relaxed max-w-4xl">
-      {{ course.description }}
+    <p
+      :class="`text-lg text-muted leading-relaxed max-w-4xl ${!hasDescription && 'italic'}`"
+    >
+      {{
+        hasDescription
+          ? course.description
+          : "Belum ada deskripsi yang tersedia untuk kursus ini."
+      }}
     </p>
   </section>
 </template>
