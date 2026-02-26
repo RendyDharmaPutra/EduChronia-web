dc3bef84f2a450b3ebc4786878847713c1360b48
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-02-26 23:32:32 +0700

    fix: fix inconsistent course name maximum length with backend validation

--- app/features/courses/course.schema.ts
+++ app/features/courses/course.schema.ts
@@ -4,7 +4,7 @@ export const courseSchema = z.object({
   name: z
     .string("Nama tidak boleh kosong")
     .min(3, "Nama minimal 3 karakter")
-    .max(255, "Nama maksimal 255 karakter"),
+    .max(100, "Nama maksimal 100 karakter"),
   description: z
     .string("Deskripsi harus berupa karakter")
     .max(255, "Deskripsi maksimal 255 karakter")
