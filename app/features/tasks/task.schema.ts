43e885f198e04c08305cb68fc6110bcb1178837d
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-04-10 18:31:12 +0700

    fix: update task schema validation error handling and constraints

--- app/features/tasks/task.schema.ts
+++ app/features/tasks/task.schema.ts
@@ -2,15 +2,16 @@ import * as z from "zod";
 
 export const taskSchema = z.object({
   title: z
-    .string("Judul tidak boleh kosong")
+    .string({ error: "Judul tidak boleh kosong" })
     .min(3, "Judul minimal 3 karakter")
-    .max(150, "Judul maksimal 150 karakter")
-    .nonoptional(),
+    .max(150, "Judul maksimal 150 karakter"),
   description: z
-    .string("Deskripsi harus berupa karakter")
+    .string({ error: "Deskripsi harus berupa karakter" })
     .max(255, "Deskripsi maksimal 255 karakter")
     .optional(),
-  deadline: z.string("Deadline tidak boleh kosong").nonoptional(),
+  deadline: z
+    .string({ error: "Tenggat waktu tidak boleh kosong" })
+    .min(1, "Tenggat waktu tidak boleh kosong"),
   isCompleted: z.boolean().default(false),
 });
 
