dfc251acbcb900b4b9acdc17ec1f701aad52be5b
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-05-06 19:02:36 +0700

    refactor: update default course list pagination limit from 10 to 9

--- app/features/courses/services/read-course-list.service.ts
+++ app/features/courses/services/read-course-list.service.ts
@@ -5,12 +5,12 @@ import type { MetaPaginationResponse } from "~/types/meta";
 
 /**
  * Service to retrieve a paginated list of courses.
- * 
+ *
  * @param page - The current page number to retrieve.
  * @param limit - The maximum number of courses to retrieve per page (defaults to 10).
  * @returns A promise returning the result of a safe API call, containing an array of courses and pagination metadata.
  */
-export const readCourseListService = async (page: number, limit = 10) =>
+export const readCourseListService = async (page: number, limit = 9) =>
   safeApiCall(() =>
     apiFetch<Course[], MetaPaginationResponse>(
       `/api/courses?page=${page}&limit=${limit}`,
