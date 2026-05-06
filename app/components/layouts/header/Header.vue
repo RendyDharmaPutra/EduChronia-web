514149ab84b6185c7d1e43cbefdc2eeec81b3f56
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-05-06 15:10:27 +0700

    feat: temporarily hide navigation menu components for MVP release

--- app/components/layouts/header/Header.vue
+++ app/components/layouts/header/Header.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-import Navbar from "./Navbar.vue";
+/**
+ * TODO [Post-MVP]: Navigation Menu Integration
+ * 
+ * For the MVP release, the navigation menu (Navbar) is temporarily hidden.
+ * In future versions, to re-enable the navigation:
+ * 1. Uncomment the `Navbar` import below.
+ * 2. Uncomment the `<Navbar>` components in the `<template>` section (slots `#right` and `#body`).
+ * 3. Adjust the `:toggle="false"` prop on `<UHeader>` to enable the mobile hamburger menu if needed.
+ */
+// import Navbar from "./Navbar.vue";
 import HeaderActions from "./HeaderActions.vue";
 import AppTitle from "~/components/AppTitle.vue";
 
@@ -10,7 +19,7 @@ const emit = defineEmits<{
 
 <template>
   <UHeader
-    mode="slideover"
+    :toggle="false"
     class="backdrop-blur-md bg-paper-bg/80 dark:bg-obsidian-900/80 border-b border-paper-outline dark:border-obsidian-100/10 transition-colors duration-200"
   >
     <template #title>
@@ -18,13 +27,15 @@ const emit = defineEmits<{
     </template>
 
     <template #right>
-      <Navbar class="lg:mr-8 xl:mr-12 hidden lg:flex" />
+      <!-- TODO [Post-MVP]: Uncomment the line below to show desktop navigation -->
+      <!-- <Navbar class="lg:mr-8 xl:mr-12 hidden lg:flex" /> -->
 
       <HeaderActions @open-sign-out-modal="emit('open-sign-out-modal')" />
     </template>
 
-    <template #body>
+    <!-- TODO [Post-MVP]: Uncomment the block below to show mobile navigation -->
+    <!-- <template #body>
       <Navbar orientation="vertical" />
-    </template>
+    </template> -->
   </UHeader>
 </template>
